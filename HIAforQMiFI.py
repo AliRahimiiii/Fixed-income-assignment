d823a0f5809e9a891d45f2a49fc9434a0441a556
Ali <amrahimi1@gmail.com>
2025-12-01 11:43:22 +0100

    made part 2 function with docstring ready for implementation

--- HIAforQMiFI.py
+++ HIAforQMiFI.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+
 
 def getTimeSeriesMean(j):
     """
@@ -15,4 +17,25 @@ def getTimeSeriesMean(j):
         The historical average (time series mean) of the specified maturity.
     """
     data = pd.read_excel('LW_monthly_1972-2024.xlsx')           # Load the data
-    return data.iloc[:, j + 1].mean().item()                    # Calculate and return the mean                        
+    return data.iloc[:, j + 1].mean().item()                    # Calculate and return the mean                   
+
+
+def getNelsonSiegelForecast(i, h, j):
+    """
+    returns a yield curve forecast using the Nelson-Siegel model for a specific maturity j at time i+h using parameters estimated up to time i.
+
+    Parameters
+    ----------
+    i : int
+        The time index up to which the subsample runs. (ranges from 12 to 612)
+    h : int
+        The forecast horizon.  (ranges from 1 to 24)   
+    j : int
+        The index of the maturity to forecast. (ranges from 1 to 120)
+    Returns
+    ----------
+    float
+        The Nelson-Siegel forecast for the specified maturity at time i+h.
+    """
+
+    return 0.0  # Placeholder for the actual implementation
